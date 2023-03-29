87e4a0c9611a95eb40fdec42222492ab5426ef2d
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2023-03-28 22:16:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,24 +20,40 @@ map.addControl(new mapboxgl.NavigationControl());
 
 
 // Fetch GeoJSON from URL and store response
-fetch('https://raw.githubusercontent.com/ananmaysharan/ggr472-lab4/main/data/pedcyc_collision_06-21.geojson')
-    .then(response => response.json())
-    .then(response => {
-        // console.log(response); //Check response in console
-        collisiongeojson = response; // Store geojson as variable using URL from fetch response
-    });
+// fetch('https://raw.githubusercontent.com/ananmaysharan/ggr472-lab4/main/data/pedcyc_collision_06-21.geojson')
+//     .then(response => response.json())
+//     .then(response => {
+//         // console.log(response); //Check response in console
+//         collisiongeojson = response; // Store geojson as variable using URL from fetch response
+//     });
+
 
 //Map Load
 map.on('load', () => {
 
+    fetch('https://raw.githubusercontent.com/ananmaysharan/ggr472-group-project/main/canfed.geojson')
+    .then(response => response.json())
+    .then(response => {
+        // console.log(response); //Check response in console
+        canfedgeojson = response; // Store geojson as variable using URL from fetch response
+    });
+
+
     //Bounding box and hexgrid creation
+    bbox2 = turf.envelope(canfedgeojson)
     bbox = turf.envelope(collisiongeojson)
 
     bboxTransformed = turf.transformScale(bbox, 1.1)
+    bboxTransformed2 = turf.transformScale(bbox2, 1.1)
 
     bboxgeojson = {
         "type": "FeatureCollection",
         "features": [bboxTransformed]
+    }
+
+    bboxgeojson2 = {
+        "type": "FeatureCollection",
+        "features": [bboxTransformed2]
     }
 
     //coords
@@ -46,15 +62,25 @@ map.on('load', () => {
     const maxX = bboxTransformed.geometry.coordinates[0][2][0];
     const maxY = bboxTransformed.geometry.coordinates[0][2][1];
 
+    const minX2 = bboxTransformed2.geometry.coordinates[0][0][0];
+    const minY2 = bboxTransformed2.geometry.coordinates[0][0][1];
+    const maxX2 = bboxTransformed2.geometry.coordinates[0][2][0];
+    const maxY2 = bboxTransformed2.geometry.coordinates[0][2][1];
+
     let bboxcords = [minX, minY, maxX, maxY]
+    let bboxcords2 = [minX2, minY2, maxX2, maxY2]
 
     const hexgrid = turf.hexGrid(bboxcords, 0.4, { units: 'kilometres' });
+    const hexgrid_two = turf.hexGrid(bboxcords2, 0.4, { units: 'kilometres' });
 
-    let collishex = turf.collect(hexgrid, collisiongeojson, '_id', 'values')
+    let collishex = turf.collect(hexgrid, collisiongeojson, '_id', 'values');
+    let canfedhex = turf.collect(hexgrid_two, canfedgeojson, 'field1', 'values');
+
 
     //console.log(collishex)
 
     let maxcollis = 0;
+    let maxcanfed = 0;
 
 
     //adding count for loop
@@ -65,13 +91,23 @@ map.on('load', () => {
         }
     });
 
+    canfedhex.features.forEach((feature) => { //iterate through features
+        feature.properties.COUNT = feature.properties.values.length //create new property count to be the length of the values array
+        if (feature.properties.COUNT > maxcanfed) { //if the count of this feature is greater than the current max number collisions
+            maxcanfed = feature.properties.COUNT //set the max collisions variable to be the count for this feature 
+        }
+    });
+
+
+
+
     //console.log(maxcollis);
 
 
     //Add datasource using GeoJSON variable
     map.addSource('toronto-col', {
         type: 'geojson',
-        data: collisiongeojson
+        data: canfedgeojson
     });
 
     map.addLayer({
@@ -109,6 +145,11 @@ map.on('load', () => {
         data: collishex
     });
 
+    map.addSource('canfed-hexgrid', {
+        type: 'geojson',
+        data: canfedhex
+    });
+
     //console.log(collishex)
 
 
@@ -128,6 +169,28 @@ map.on('load', () => {
                 maxcollis, colorScheme[colorScheme.length - 1]
             ],
             'fill-opacity': 0.3
+        },
+        'layout': {
+            'visibility': 'none',
+        }
+    });
+
+    map.addLayer({
+        'id': 'canfed-hexgrid',
+        'type': 'fill',
+        'source': 'canfed-hexgrid',
+        'paint': {
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'COUNT'],
+                0, colorScheme[0],
+                maxcanfed, colorScheme[colorScheme.length - 1]
+            ],
+            'fill-opacity': 0.3
+        },
+        'layout': {
+            'visibility': 'visible',
         }
     });
     
